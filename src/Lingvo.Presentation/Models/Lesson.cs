125fa490f4f7f098c10414bfe844514874680c07
Tiago Douglas <tiago.santos@consorcioluiza.com.br>
2022-10-25 22:14:14 -0300

    Create initial lesson layout

---
+++ src/Lingvo.Presentation/Models/Lesson.cs
@@ -0,0 +1,12 @@
+﻿namespace Lingvo.Presentation.Models;
+
+public class Lesson
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Language { get; set; }
+    public Guid UserId { get; set; }
+    public Guid LanguageId { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime? DateUpdated { get; set; }
+}
